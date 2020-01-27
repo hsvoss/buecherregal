3d22fcbcbcb5170f0e214d2834c660892cc27327
Hendrik Sebastian Voß <hendrik.voss@tu-dortmund.de>
2020-01-27 20:09:46 +0100

    Refactored Book-Style to CSS and a more readable method

--- src/readinghall/Book.js
+++ src/readinghall/Book.js
@@ -2,16 +2,22 @@ import React from "react";
 import BookshelfChanger from "./BookshelfChanger";
 import PropTypes from "prop-types";
 
+const findBackGroundImageUrl = book => {
+    if (book.imageLinks && book.imageLinks.thumbnail) {
+        return (book.imageLinks.thumbnail);
+    } else {
+        return "";
+    }
+};
 
 const Book = props => (
     <div className="book">
         <div className="book-top">
             <div className="book-cover" style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${(props.book.imageLinks && props.book.imageLinks.thumbnail) ? props.book.imageLinks.thumbnail : ""})`
-            }} />
-            <BookshelfChanger moveToShelf={(event) => props.moveToShelf(event.target.value, props.book)} currentShelf={props.book.shelf || 'none'} />
+                backgroundImage: `url(${findBackGroundImageUrl(props.book)})`
+            }}/>
+            <BookshelfChanger moveToShelf={(event) => props.moveToShelf(event.target.value, props.book)}
+                              currentShelf={props.book.shelf || 'none'}/>
         </div>
         <div className="book-title">{props.book.title}</div>
         <div className="book-authors">{props.book.authors ? props.book.authors[0] : "No Author"}</div>
